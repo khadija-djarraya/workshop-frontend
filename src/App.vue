c22df86cdaa4c4c1a481210d240c42fbdf685c24
khadija-djarraya <khadija.djarraya@medtech.tn>
2022-04-15 15:32:40 +0100

    latest update

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,16 @@
   >
     <v-list three-line>
 
-        <v-list-item v-for="movie in movies" :key ="movie">
+        <v-list-item v-for="movie in movies" :key ="movie.id"><!--vue needs to associate a unique id to each movie, that is the role of ":key"-->
           <v-list-item-content>
             <v-list-item-title>{{movie.Title}}</v-list-item-title> <!-- this is the title of the card -->
             <v-list-item-subtitle>{{movie.Subtitle}} </v-list-item-subtitle> <!-- this is the subtitle of the card -->
           </v-list-item-content>
+          <v-list-item-action>
+          <v-btn icon @click="deleteMovie(movie.id)">
+            <v-icon small color="red darken-4">mdi-delete</v-icon><!--using words is better than hex-->
+          </v-btn>
+        </v-list-item-action>
         </v-list-item>
 
     </v-list>
@@ -39,6 +44,17 @@ export default {
     //put line 37 in a variable, always do that when you don't know the content of a line.
     console.log("finished mounting.",res.data);
     this.movies=res.data;
+  },
+  methods:{
+    deleteMovie(id){
+      alert("delete movie "+id);
+      const res= await axios.delete("http://localhost:3000/movies/"+id);
+      this.refreshMovies();
+    },
+    refreshMovies(){
+      const res= await axios.get("http://localhost:3000/movies/");
+      console.log("finish.")
+    }
   }
 };
 </script>
