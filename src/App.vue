8984cd5c93edeae4a9e75cb9d56473d9a4a03444
khadija-djarraya <khadija.djarraya@medtech.tn>
2022-04-15 14:09:28 +0100

    a second commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    hello world
+  <v-card
+    max-width="450"
+    class="mx-auto"
+  >
+    <v-list three-line>
+
+        <v-list-item v-for="movie in movies" :key ="movie">
+          <v-list-item-content>
+            <v-list-item-title>{{movie.Title}}</v-list-item-title> <!-- this is the title of the card -->
+            <v-list-item-subtitle>{{movie.Subtitle}} </v-list-item-subtitle> <!-- this is the subtitle of the card -->
+          </v-list-item-content>
+        </v-list-item>
+
+    </v-list>
+  </v-card>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+  },
+
+  data: () => ({
+    movies:[]
+  }),
+  mounted(){
+
+  }
+};
+</script>
